3c8f4594515579dc374390a54c232f1de804679c
jasmin <jasminchakma895@gmail.com>
2023-11-03 10:03:04 +0600

    added product by id route

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nkafzub.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,6 +37,19 @@ async function run() {
         .toArray();
       res.send(result);
     });
+
+    app.post('/productsById', async (req, res) => {
+
+      const ids = req.body;
+      const objectIds = ids.map(id => new ObjectId(id) );
+      const query = {
+        _id : {
+          $in : objectIds
+        }
+      }
+      const result = await productsCollection.find(query).toArray()
+      res.send(result)
+    })
 
     app.get("/productsCount", async (req, res) => {
       const count = await productsCollection.estimatedDocumentCount();
